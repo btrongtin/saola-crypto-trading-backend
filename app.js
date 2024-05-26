7630b427fcc96d6d308e7c3760bc4253273f76a5
TinBui <btrongtin891@gmail.com>
2024-05-26 19:02:22 +0700

    Add docker files

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ fastify.register(accountsService, { prefix: 'api/accounts' });
 fastify.register(transactionsService, { prefix: 'api/transactions' });
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
